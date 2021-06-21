0d24d9c05ca9f5adad9b11cde345358949e84f99
Electron1997 <63196848+Electron1997@users.noreply.github.com>
2021-06-21 13:33:13 +0200

    Template

--- Mathematics/number_theory.cpp
+++ Mathematics/number_theory.cpp
@@ -1,1 +1,69 @@
+#include <bits/stdc++.h>
 
+#define f first
+#define s second
+#define loop(i, n) for (int i = 0; i < n; ++i)
+#define read(a, n) loop($, n) cin >> a[$];
+#define show(a, n)                  \
+    loop($, n) cout << a[$] << " "; \
+    cout << endl;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*	// RANDOM NUMBER GENERATOR
+// rng() generates u.a.r. from [0, 2^32 - 1]
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+
+/* // TIME
+auto start = chrono::high_resolution_clock::now();
+// ...
+auto stop = chrono::high_resolution_clock::now();
+auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+cout << duration.count() << endl;
+*/
+
+// CRT
+
+// EEA
+
+// Time: O(sqrt(n))
+map<ull, int> factor(ull n){
+    ;
+}
+
+// Time: 
+ull gcd(ull a, ull b){
+    ;
+}
+
+ull lcm(ull a, ull b){
+    ;
+}
+
+// Time: O(sqrt(n))
+bool prime(ull n){
+    ;
+}
+
+// Time:
+void sieve(){
+    ;
+}
+
+int main(){
+	/*	// IO
+	ios_base::sync_with_stdio(false);	// unsync C- and C++-streams (stdio, iostream)
+	cin.tie(NULL);	// untie cin from cout (no automatic flush before read)
+	*/
+    int T;
+    cin >> T;
+    loop(t, T){
+        int l, r;
+        cin >> l >> r;
+    }
+    return 0;
+}
