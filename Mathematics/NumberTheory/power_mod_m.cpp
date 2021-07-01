db7d7b3f7e5828a2c6651464a67a574ee71d1432
Electron1997 <63196848+Electron1997@users.noreply.github.com>
2021-07-01 21:07:47 +0200

    Create power_mod_m.cpp

---
+++ Mathematics/NumberTheory/power_mod_m.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define f first
+#define s second
+#define loop(i, n) for (int i = 0; i < n; ++i)
+#define read(a, n) loop($, n) cin >> a[$];
+#define show(a, n)                  \
+    loop($, n) cout << a[$] << " "; \
+    cout << endl;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*  // RANDOM NUMBER GENERATOR
+// rng() generates u.a.r. from [0, 2^32 - 1]
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+
+// Time: O(log(n))
+ull power(ull a, ull b, ull m){
+    ull p = 1;
+    a %= m;
+    while(b){
+        if(b & 1){
+            p *= a;
+            p %= m;
+        }
+        a *= a;
+        a %= m;
+        b >>= 1;
+    }
+    return p;
+}
+
+int main(){
+    /*
+    auto start = chrono::high_resolution_clock::now();
+    */
+    
+    ios_base::sync_with_stdio(false);   // unsync C- and C++-streams (stdio, iostream)
+    cin.tie(NULL);  // untie cin from cout (no automatic flush before read)
+
+    int T;
+    cin >> T;
+    loop(t, T){
+        ull a, b, m;
+        cin >> a >> b >> m;
+        cout << power(a, b, m) << endl;
+    }
+
+    /*
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count() << endl;
+    */
+    return 0;
+}
