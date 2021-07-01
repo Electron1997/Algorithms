22c265c274e223a4331e5df85cde2f67031c7836
Electron1997 <63196848+Electron1997@users.noreply.github.com>
2021-07-01 21:03:12 +0200

    Create gcd_and_lcm.cpp

---
+++ Mathematics/NumberTheory/gcd_and_lcm.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+#define f first
+#define s second
+#define loop(i, n) for (int i = 0; i < n; ++i)
+#define read(a, n) loop($, n) cin >> a[$];
+#define show(a, n)                  \
+    loop($, n) cout << a[$] << " "; \
+    cout << endl;
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+/*  // RANDOM NUMBER GENERATOR
+// rng() generates u.a.r. from [0, 2^32 - 1]
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+*/
+
+// Time: O(log(ab))
+ull gcd(ull a, ull b){
+    if(a < b){
+        swap(a, b);
+    }
+    while(b){
+        ull r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+// Time: O(log(ab))
+ull lcm(ull a, ull b){
+    return a / gcd(a, b) * b;
+}
+
+int main(){
+    /*
+    auto start = chrono::high_resolution_clock::now();
+    */
+    
+    ios_base::sync_with_stdio(false);   // unsync C- and C++-streams (stdio, iostream)
+    cin.tie(NULL);  // untie cin from cout (no automatic flush before read)
+
+    int T;
+    cin >> T;
+    loop(t, T){
+        ull a, b;
+        cin >> a >> b;
+        cout << gcd(a, b) << endl;
+    }
+
+    /*
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count() << endl;
+    */
+    return 0;
+}
