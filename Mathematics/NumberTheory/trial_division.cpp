7cd6196c647a61e5cfdba8d4a690d6be48c1df47
Electron1997 <63196848+Electron1997@users.noreply.github.com>
2021-07-01 20:47:57 +0200

    Update and rename number_theory.cpp to trial_division.cpp

--- Mathematics/NumberTheory/trial_division.cpp
+++ Mathematics/NumberTheory/trial_division.cpp
@@ -13,22 +13,10 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-/*	// RANDOM NUMBER GENERATOR
+/*  // RANDOM NUMBER GENERATOR
 // rng() generates u.a.r. from [0, 2^32 - 1]
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 */
-
-/* // TIME
-auto start = chrono::high_resolution_clock::now();
-// ...
-auto stop = chrono::high_resolution_clock::now();
-auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-cout << duration.count() << endl;
-*/
-
-// CRT
-
-// EEA
 
 // Time: O(sqrt(n))
 map<ull, int> *factor(ull n){
@@ -49,35 +37,31 @@ map<ull, int> *factor(ull n){
     return factorization;
 }
 
-// Time: 
-ull gcd(ull a, ull b){
-    ;
-}
-
-ull lcm(ull a, ull b){
-    ;
-}
-
-// Time: O(sqrt(n))
-bool prime(ull n){
-    ;
-}
-
-// Time:
-void sieve(){
-    ;
-}
-
 int main(){
-	/*	// IO
-	ios_base::sync_with_stdio(false);	// unsync C- and C++-streams (stdio, iostream)
-	cin.tie(NULL);	// untie cin from cout (no automatic flush before read)
-	*/
+    /*
+    auto start = chrono::high_resolution_clock::now();
+    */
+    
+    ios_base::sync_with_stdio(false);   // unsync C- and C++-streams (stdio, iostream)
+    cin.tie(NULL);  // untie cin from cout (no automatic flush before read)
+
     int T;
     cin >> T;
     loop(t, T){
-        int l, r;
-        cin >> l >> r;
+        ull n;
+        cin >> n;
+        map<ull, int> factorization = *(factor(n));
+        cout << n << " = ";
+        for(pair<ull, int> p : factorization){
+            cout << p.f << "^" << p.s << " ";
+        }
+        cout << endl;
     }
+
+    /*
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count() << endl;
+    */
     return 0;
 }
