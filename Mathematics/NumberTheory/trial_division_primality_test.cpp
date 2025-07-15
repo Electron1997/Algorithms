f99ce650367bfd9d67b8b5f7afbbd25aefa12456
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2025-07-15 17:35:11 +0200

    Replace int with ull to avoid overflow

--- Mathematics/NumberTheory/trial_division_primality_test.cpp
+++ Mathematics/NumberTheory/trial_division_primality_test.cpp
@@ -25,7 +25,7 @@ bool prime(ull n){
     if(n % 2 == 0){
         return false;
     }
-    for(int d = 3; d * d <= n; d += 2){
+    for(ull d = 3; d * d <= n; d += 2){
         if(n % d == 0){
             return false;
         }
